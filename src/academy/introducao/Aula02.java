310dfa471ef311ede1e4451c23941874d1e7deff
Igor Sousa <igor.sousa00524@gmail.com>
2025-10-27 21:22:15 -0300

    Commit test

--- src/academy/introducao/Aula02.java
+++ src/academy/introducao/Aula02.java
@@ -2,6 +2,18 @@ package academy.introducao;
 
 public class Aula02 {
     public static void main(String[] args) {
-        System.out.println("KA-ME-HA-ME-HA!!!!");
+        // tipos de dados: int, float, char, byte, short, long, boolean
+        int idade = 10;
+        long numeroGrand = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+        System.out.println("A idade do pacinete X é " +idade);
+        System.out.println(falso);
+        System.out.println("char "+caractere);
+
     }
 }
