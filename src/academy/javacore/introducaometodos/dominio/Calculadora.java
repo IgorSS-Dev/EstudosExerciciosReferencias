1c8e84f5403ecfa8877ed02c9d5615d5f03c3be5
Igor Sousa <igor.sousa00524@gmail.com>
2026-08-04 21:48:36 -0300

    doc: Alterei alguns metodos de uma classe e adicionei novos comentários.

--- src/academy/javacore/introducaometodos/dominio/Calculadora.java
+++ src/academy/javacore/introducaometodos/dominio/Calculadora.java
@@ -3,18 +3,19 @@ package academy.javacore.introducaometodos.dominio;
 public class Calculadora {
     // Declaração de metodo
     public void somaDoisNumeros() {
-        System.out.println(10+10);
+        System.out.println(10 + 10);
     }
 
     public void somaSubtraiNumeros() {
-        System.out.println(21-2);
+        System.out.println(21 - 2);
     }
 
     // Exemplo de parâmetro
     public void multiplicaDoisNumeros(int num1, int num2) {
         System.out.println(num1 * num2);
     }
-        //Exemplo de metodo com retorno
+
+    //Exemplo de metodo com retorno
     public double divideDoisNumeros(double num1, double num2) {
         if (num2 == 0) {
             return 0;
@@ -22,4 +23,23 @@ public class Calculadora {
         return num1 / num2;
     }
 
+    // Exemplo de uso diferente de retur
+    public double divideDoisNumeros02(double num1, double num2) {
+        if (num2 != 0) {
+            return 0;
+        } else {
+            return num1 / num2;
+        }
+
+    }
+
+    // Exemplo de return com metodo void
+    public void imprimeDivisaoDeDoisNumeros(double num1, double num2) {
+        if (num2 == 0) {
+            System.out.println("Não existe divisão por zero");
+            // Nesse caso, utilizei uma mensagem na condicional, pois o comando "return" não é válido para metodo void
+        } else {
+            System.out.println(num1 / num2);
+        }
+    }
 }
--- src/academy/javacore/introducaometodos/test/CalculadoraTest03.java
+++ src/academy/javacore/introducaometodos/test/CalculadoraTest03.java
@@ -7,5 +7,7 @@ public class CalculadoraTest03 {
         Calculadora calculadora3 = new Calculadora();
         double resultado = calculadora3.divideDoisNumeros(66, 0);
         System.out.println(resultado);
+        System.out.println(calculadora3.divideDoisNumeros02(20, 0));
+        calculadora3.imprimeDivisaoDeDoisNumeros(99, 0);
     }
 }
